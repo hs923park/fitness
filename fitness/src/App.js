7b97af34c8c1fcc68a51089959134ac831674933
hs923park <hs923park@gmail.com>
2023-07-16 23:56:06 +0900

    start

---
+++ fitness/src/App.js
@@ -0,0 +1,23 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        운동
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        <button className="record">기록</button>
+        <button className="gym">운동</button>
+        {/* <button style={{ backgroundImage: "url(/gym.png)" }}>운동</button> */}
+        <button className="profile">프로필</button>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
